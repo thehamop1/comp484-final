4d4af5575206207aea589db5cb71669f62253a8f
thehamop1 <cristian.gutierrez.56@my.csun.edu>
2019-11-09 18:24:19 -0800

    adding some stuff for the professor and presentation

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const searchURL = "https://api.github.com/search/";
-
+const REPO = "Repo";
+const USER = "User"
+//this just makes a request for the github data
 function getData(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.onload = function(variable) {
@@ -16,40 +18,55 @@ function getData(url, callback) {
     xhr.send();
 }
 
-function createOptions(results){//this is just populating the front end for now
-    var listFromGithub = results.items;
-    var listOfResults = document.getElementById("listOfResults");
-    var searchType = document.getElementById("searchType").value;
-    console.log(results);
-    for(var result=0;result<listFromGithub.length;result++){
-        var option = document.createElement("option");
-        option.value = listFromGithub[result].url;
-        option.innerHTML = listFromGithub[result].name;
-        option.setAttribute('owner', listFromGithub[result].owner.login);
-        listOfResults.append(option);
-    }
-    listOfResults.setAttribute('searchType', searchType);
-}
-function createData(repoData){
-    var body = document.getElementsByTagName('body');
-    var summary = document.createElement("p");
-    summary.innerHTML = repoData.description;
-    console.log(repoData);
-    document.body.append(summary);
-    
-}
 function searchResults(){
     var selection = document.getElementById("listOfResults").value;
     console.log(selection);
-    getData(selection, createData);
-    //getData(selection.collaborators_url, createData);
+    //getData(selection, writeSearchDetails);
 }
-
+//this is the first initial search when the user wants to get a list of possible matches
+function writeToDom(aString){
+    var writer = document.createElement("p");
+    writer.innerHTML = aString;
+    document.body.append(writer);
+}
+function writeSearchDetails(repoData){//THIS IS TEMPORARY AND NEEDS TO BE REPLACED BY REACT.JS
+    var body = document.getElementsByTagName('body');
+    var writer = document.createElement("p");
+    var isRepoSearch = (document.getElementById("searchType").value===REPO);
+    if(isRepoSearch){
+        writeToDom("Owner: " + repoData.owner.login);
+        writeToDom("Summary: " + repoData.description);
+    }else{
+        console.log("hello world");
+    }
+    writeToDom("-----------------------------------------------------------------------------------------------------");
+}
+function getUserListFromApiData(apiData, listHTML){
+    console.log("hello world");
+}
+function getRepoListFromApiData(apiData, listHTML){
+    for(var result=0;result<apiData.length;result++){
+        var option = document.createElement("option");
+        option.value = apiData[result].url;
+        option.innerHTML = apiData[result].name;
+        option.setAttribute('owner', apiData[result].owner.login);
+        listOfResults.append(option);
+    }
+    listOfResults.setAttribute('searchType', searchType);
+    for(var result=0;result<10;result++)
+        getData(apiData[result].url, writeSearchDetails);
+}
+function createOptions(results){//this is just populating the front end for now
+    var listFromGithub = results.items;
+    var listOfResultsHTML = document.getElementById("listOfResults");
+    var isRepoSearch = (document.getElementById("searchType").value===REPO);
+    (isRepoSearch) ? getRepoListFromApiData(listFromGithub, listOfResultsHTML): getUserListFromApiData(listFromGithub, listOfResultsHTML);
+}
 function Search(){
     var keyword = document.getElementById("Search").value;
     var searchType = document.getElementById("searchType").value;
-    if(searchType==="User")
+    if(searchType===USER)
         getData(searchURL + "users?q=" + keyword, createOptions);
-    else if(searchType === "Repo")
+    else if(searchType ===REPO)
         getData(searchURL + "repositories?q=" + keyword, createOptions);
 }
